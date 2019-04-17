5bc7669c92bfcf0a568c41850a34881dcf4dadce
mrozo2m <mrozo2m@interia.pl>
2019-04-17 10:28:49 +0200

    syncing users

--- src/Users.js
+++ src/Users.js
@@ -4,20 +4,26 @@ import { connect } from 'react-redux'
 import { setUsersActionCreator } from './state/users'
 
 const Users = (props) => {
+    if(props._users === null){
     fetch ('https://randomuser.me/api')
         .then(r => r.json())
         .then(data => {
             props._setUsers(data.results)
         })
+    }
 
     return (
         < div >
-            Users
+            {
+                
+            }
         </div >
     )
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+    _users: state.users.users
+})
 
 const mapDispatchToProps = dispatch => ({
     _setUsers: (users) => dispatch(setUsersActionCreator(users))
